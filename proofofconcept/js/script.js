e78913fc18ee5b6e5eaf2772e4db076a56aed7aa
Troy Grant <Tr5978@gmail.com>
2012-04-17 01:34:17 -0400

    Successfully communicating with Mongo

--- proofofconcept/js/script.js
+++ proofofconcept/js/script.js
@@ -7,12 +7,12 @@
 
 	var apiKey = "xvien5ya8rh538ryt5kh",
 		conceptList = $("#conceptList"),
-		callServerBtn = $("#callServerBtn")
+		callServerBtn = $("#callServerBtn"),
+		needsAssistanceToggle = false
 	;
 
 
 	function fetchMongoData(url, query, doc, method, callback){
-		console.log('fuck');
 		var mongoDataObj = {}; //creating the mongoData Object
   		//var url =  https://api.mongohq.com/databases/xcusemedb/collections/xcusemedata
   		/*
@@ -43,9 +43,70 @@
 		console.log("Mongo Was Successful, here's what it's getting back: ", response[0].needsAssistance); 
 		//the response is an array of objects so an index of 0 has to be specified
 
+		//change the local boolean to know what's up
+		needsAssistanceToggle = response[0].needsAssistance;
+
 		//letting the status of the table be displayed
-		var tableStatus = (response[0].needsAssistance == "true") ? "Server Requested" : "Normal";
+		var tableStatus = (needsAssistanceToggle == true) ? "Server Requested" : "Normal";
 		conceptList.append('<li>Table Status: '+ tableStatus +'</li>');
+	};
+
+	function notifyServer(url, query, doc, method, callback){
+		var mongoDataObj = {}; //creating the mongoData Object
+  		//var url =  https://api.mongohq.com/databases/xcusemedb/collections/xcusemedata
+  		/*
+  		http://support.mongohq.com/api/documents/index.html
+		The base API end point is https://api.mongohq.com and you will pass your key as a params named _apikey for authentication.
+		*/
+		//PARAMS USED FOR A PUT REQUEST (aka update by criteria, bulk update)
+		
+		//_apikey Your MongoHQ account’s secret key.
+		mongoDataObj._apikey = apiKey;
+		//db The database name. (in the URL)
+		//col The collection name. (in the URL)
+
+		//criteria The JSON criteria used to match which documents to update
+		mongoDataObj.criteria = {
+			"type" : "table" //this is temporary, have it select by unique tableId later
+		};
+		/*object The JSON document update command (called objNew in the MongoDB docs. 
+		Accepts standard MongoDB update directives, like $set or $inc).*/
+		//This is pretty much a shitty way of saying what the fuck you wanna update
+		mongoDataObj.object = {
+			//toggle the needsAssistance boolean in mongo. 
+			//If it's false, make it true, if true make it false.
+			"$set" : {"needsAssistance" : (needsAssistanceToggle == true) ? false : true} //objects within objects, mofos
+		};
+
+  		//mongoDataObj.document = doc;
+
+  		$.ajax({
+      		url: url,
+      		type: method,
+		    dataType: 'json',
+		    data: mongoDataObj,
+		    success: callback,
+		    error: mongoError
+   		 });
+
+	};
+
+	function processServerRequest(response){
+		var tableStatus; //setting up this var
+		//this runs on the ajax success
+		console.log("The Request Has Been Made: ", response);
+		//letting the status of the table be displayed
+		if(response.ok == 1){
+			//if the response.ok is 1, the data was sucessfully changed in mongo.
+			//No need to grab the new data, we already have it in our local variable.
+			//since this is a toggle, it'll be the opposite of whatever it was.
+			needsAssistanceToggle = (needsAssistanceToggle == true) ? false : true;
+			tableStatus = (needsAssistanceToggle == true) ? "Server Requested" : "Normal";
+			conceptList.append('<li>Table Status: '+ tableStatus +'</li>');
+		}else{
+			console.log("Something's up with the response.ok");
+		};
+		
 	};
 
 
@@ -56,8 +117,15 @@
 			"type": "table" //get the data where the table is number 5
 		};
 		fetchMongoData("https://api.mongohq.com/databases/xcusemedb/collections/xcusemedata/documents",mongoQuery,{},"GET",processInformation);
-		//documents is only at the end of this because it was on the example
 		//url, query, doc, method, callback
+
+		//listener for the button
+		callServerBtn.on("click", function(event){
+			alert("Your server will be with you shortly.");
+			notifyServer("https://api.mongohq.com/databases/xcusemedb/collections/xcusemedata/documents",mongoQuery,{},"PUT", processServerRequest);
+			//PUT is used here because we're altering the database
+		});
+
 		
 	}); // end document ready
 	
