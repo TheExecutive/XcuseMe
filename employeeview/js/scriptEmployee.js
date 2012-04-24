46fa37b25400a2e013fb34b61a5c3541e430fa70
Marcos DeSousa <reddoggato@gmail.com>
2012-04-23 22:47:08 -0400

    dashboard popup updated

--- employeeview/js/scriptEmployee.js
+++ employeeview/js/scriptEmployee.js
@@ -105,8 +105,8 @@
 		});
 
 		//run this every 8 seconds.
-		//setInterval( monitorRemoteApp, 8000);
-		monitorRemoteApp();
+		setInterval( monitorRemoteApp, 8000);
+		//monitorRemoteApp();
 		
 	}
 
